2091d63100c189ebf6356703e0280f857f253d9d
Aditya Raj <adistrim.dev@gmail.com>
2025-08-04 22:17:54 +0530

    removed queryClient

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
-const queryClient = new QueryClient()
 const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
@@ -19,9 +17,7 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <RouterProvider router={router} />
     </StrictMode>,
   );
 }
