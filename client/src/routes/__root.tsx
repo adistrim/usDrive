d8ed13127e8503843d2f1c123e3828a6a56e7f2b
Rishika Bothra <rshkbothra@gmail.com>
2025-08-04 21:55:00 +0530

    init react

---
+++ client/src/routes/__root.tsx
@@ -0,0 +1,11 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+
+export const Route = createRootRouteWithContext<{queryClient : QueryClient}>()(
+    {component : RootComponent}
+
+    
+)
+function RootComponent(){
+    return <Outlet/>
+}
