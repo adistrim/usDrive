e6fdc6940f55e7d24ab64eb974abad1e66f8dcbc
Rishika Bothra <rshkbothra@gmail.com>
2025-08-06 00:03:41 +0530

    feat:created ui to upload files

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,5 +1,16 @@
 import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 
 export const Route = createFileRoute('/')({
     component: RouteComponent,
@@ -7,9 +18,28 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
     return (
-        <div>
-            <Button>click me</Button>
-            Hello "/"!
-        </div>
-    )
+    <div className="flex justify-end p-5 top-0 items-center pr-16">
+      <Dialog>
+        <form>
+          <DialogTrigger asChild>
+            <Button className="cursor-pointer ">Upload files</Button>
+          </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Upload your files here!</DialogTitle>
+            <div>
+                <Label htmlFor="file-upload">File</Label>
+                <Input id="file-upload" type="file" />
+            </div>
+          </DialogHeader>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="submit">Upload</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+    </div>
+  )
 }
