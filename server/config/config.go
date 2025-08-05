66158a7015f0b55dd56d5940575e2876f12f2b9e
Aditya Raj <adistrim.dev@gmail.com>
2025-08-05 09:37:04 +0530

    export databaseURL from config

--- server/config/config.go
+++ server/config/config.go
@@ -1,13 +1,16 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"os"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	Port string
+	DatabaseURL string
 }
 
 var ENV *Config
@@ -29,8 +32,14 @@ func LoadConfig() (*Config, error) {
 		port = "8080"
 	}
 	
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
+	}
+	
 	return &Config{
 		Port: port,
+		DatabaseURL: databaseURL,
 	}, nil
 }
 
